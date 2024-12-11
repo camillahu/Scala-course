5216a422286f323b9bec5ef1562cabe249f14d1f
camillahu <camillahaugenurdal@gmail.com>
2024-12-11 13:12:23 +0100

    Update OOBasics.scala

--- src/main/scala/workbook/part2oop/OOBasics.scala
+++ src/main/scala/workbook/part2oop/OOBasics.scala
@@ -8,17 +8,31 @@ object OOBasics {
     val allCaps = name.toUpperCase()
 
     // methods
-    def greet(name: String): Unit = {
-      println(s"$name says: Hi, $name")
-    }
+    def greet(name: String): String =
+      s"${this.name} says: Hi, $name"
+    // this.name refers to the name in the Person class
+    // name refers to the name parameter of the greet method.
+
+    //overload method- different method signature, this one takes no arguments.
+    //an overload method has to have either different argument types or number of arguments.
+    def greet(): String =
+      s"Hi, everyone. My name is $name"
+      //this. keyword is not needed in this instance, the compiler understands what name is referring to.
+
+    // aux constructor (quite rarely used, basically the same as default values.)
+    def this(name: String) =
+      this(name, 0)
+
+    def this() =
+      this("Jane Doe")
   }
 
   val aPerson: Person = new Person("John", 26)
   val john = aPerson.name // class parameter is not the same as class field in Scala.
   val johnYelling = aPerson.allCaps
-
+  val johnSayHiToDaniel = aPerson.greet("Daniel")
 
   def main(args: Array[String]): Unit = {
-    aPerson.greet("camilla")
+    println(johnSayHiToDaniel)
   }
 }
