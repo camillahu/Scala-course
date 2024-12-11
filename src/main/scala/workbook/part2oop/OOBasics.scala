6f76317c1f7bebbfd43000c4af17e8b016055230
camillahu <camillahaugenurdal@gmail.com>
2024-12-11 12:50:38 +0100

    Update OOBasics.scala

--- src/main/scala/workbook/part2oop/OOBasics.scala
+++ src/main/scala/workbook/part2oop/OOBasics.scala
@@ -2,10 +2,23 @@ package workbook.part2oop
 
 object OOBasics {
 
-  class Person(name:String, age:Int)
+  //classes
+  class Person(val name:String, val age:Int) { // constructor signature
+    //fields
+    val allCaps = name.toUpperCase()
+
+    // methods
+    def greet(name: String): Unit = {
+      println(s"$name says: Hi, $name")
+    }
+  }
+
+  val aPerson: Person = new Person("John", 26)
+  val john = aPerson.name // class parameter is not the same as class field in Scala.
+  val johnYelling = aPerson.allCaps
 
 
   def main(args: Array[String]): Unit = {
-
+    aPerson.greet("camilla")
   }
 }
