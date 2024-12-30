6b9f676dec9bad7f45dac27cb13b07e4b52abe33
camillahu <camillahaugenurdal@gmail.com>
2024-12-30 15:03:56 +0100

    Update LList.scala

--- src/main/scala/workbook/practise/LList.scala
+++ src/main/scala/workbook/practise/LList.scala
@@ -1,50 +1,68 @@
 package workbook.practise
+
 import scala.annotation.tailrec
 import scala.util.Try
 
 //singly linked list
-abstract class LList[A]   {
-  def head : A
+abstract class LList[A] {
+  def head: A
+
   def tail: LList[A]
+
   def isEmpty: Boolean
-  def add(element:A): LList[A] = Cons(element, this)
+
+  def add(element: A): LList[A] = Cons(element, this)
 
   infix def ++(anotherList: LList[A]): LList[A]
 
   def map[B](transformer: A => B): LList[B]
+
   def filter(predicate: A => Boolean): LList[A]
+
   def flatMap[B](transformer: A => LList[B]): LList[B]
+
   def foreach(applyUnit: A => Unit): Unit
+
+  def sort(sorter: (A, A) => Int): LList[A]
 }
 
 case class Empty[A]() extends LList[A] {
   override def head: A = throw new NoSuchElementException("Empty list has no head")
+
   override def tail: LList[A] = throw new UnsupportedOperationException(("Empty list has no tail"))
+
   override def isEmpty: Boolean = true
+
   override def toString = "[]"
 
-  override infix def ++(anotherList: LList[A]):LList[A] = anotherList
+  override infix def ++(anotherList: LList[A]): LList[A] = anotherList
 
   override def map[B](transformer: A => B): LList[B] = Empty[B]()
+
   override def filter(predicate: A => Boolean): LList[A] = this
+
   override def flatMap[B](transformer: A => LList[B]): LList[B] = Empty[B]()
+
   override def foreach(applyUnit: A => Unit): Unit = println()
+
+  override def sort(sorter: (A, A) => Int): LList[A] = this
 }
 
-case class Cons[A](head:A, tail: LList[A]) extends LList[A] {
+case class Cons[A](head: A, tail: LList[A]) extends LList[A] {
   override def isEmpty: Boolean = false
-  override def add(element:A): LList[A] = new Cons[A](element, this)
+
+  override def add(element: A): LList[A] = new Cons[A](element, this)
 
   override def toString = {
     @tailrec
     def concatenateElements(remainder: LList[A], acc: String): String =
-      if(remainder.isEmpty) acc
+      if (remainder.isEmpty) acc
       else concatenateElements(remainder.tail, s"$acc ${remainder.head}")
 
     s"[${concatenateElements(tail, s"$head")}]"
   }
 
-  override infix def ++(anotherList: LList[A]):LList[A] = {
+  override infix def ++(anotherList: LList[A]): LList[A] = {
     Cons(head, tail ++ anotherList)
   }
 
@@ -52,12 +70,12 @@ case class Cons[A](head:A, tail: LList[A]) extends LList[A] {
     Cons(transformer(head), tail.map(transformer))
   }
 
-  override def filter(predicate:A => Boolean): LList[A] = {
-    if(predicate(head)) Cons(head, tail.filter(predicate))
+  override def filter(predicate: A => Boolean): LList[A] = {
+    if (predicate(head)) Cons(head, tail.filter(predicate))
     else tail.filter(predicate)
   }
 
-  override def flatMap[B](transformer:A => LList[B]): LList[B] = {
+  override def flatMap[B](transformer: A => LList[B]): LList[B] = {
     transformer(head) ++ tail.flatMap(transformer)
   }
 
@@ -65,39 +83,53 @@ case class Cons[A](head:A, tail: LList[A]) extends LList[A] {
     applyTo(head)
     tail.foreach(applyTo)
   }
-}
 
-object LList {
-  def find[A](list: LList[A], predicate: A => Boolean): A = {
-    if(list.isEmpty) throw new NoSuchElementException
-    else if (predicate(list.head)) list.head
-    else find(list.tail, predicate)
+  override def sort(sorter: (A, A) => Int): LList[A] = {
+    def insert(thisElement: A, sortedList: LList[A]): LList[A] = {
+      if (sortedList.isEmpty || sorter(thisElement, sortedList.head) <= 0) Cons(thisElement, sortedList)
+      else Cons(sortedList.head, insert(thisElement, sortedList.tail))
+    }
+    if (tail.isEmpty) this
+    else insert(head, tail.sort(sorter))
   }
 }
 
-object LListTest {
-
-  def main(args: Array[String]): Unit = {
-
-    //NEW TESTS
-    val intList: LList[Int] = Cons(1, Cons(2, Cons(3, Cons(4, Empty()))))
-
-    //map test
-    val doubler_v2: Int => Int = _ * 2
-    println(intList.map(doubler_v2))
-
-    //filter test
-    val evenPredicate_v2: Int => Boolean = _ % 2 == 0
-    println(intList.filter(evenPredicate_v2))
-
-    //flatmap test
-    val doublerList_v2: Int => LList[Int] = x => Cons(x, Cons(x * 2, Empty()))
-    println(intList.flatMap(doublerList_v2))
-
-    //foreach test
-    val printHeart: Int => Unit = x => print(s"$x <3 ")
-    intList.foreach(printHeart)
+  object LList {
+    def find[A](list: LList[A], predicate: A => Boolean): A = {
+      if (list.isEmpty) throw new NoSuchElementException
+      else if (predicate(list.head)) list.head
+      else find(list.tail, predicate)
+    }
   }
+
+  object LListTest {
+
+    def main(args: Array[String]): Unit = {
+
+      //NEW TESTS
+      val intList: LList[Int] = Cons(1, Cons(2, Cons(3, Cons(4, Empty()))))
+
+      //map test
+      val doubler_v2: Int => Int = _ * 2
+      println(intList.map(doubler_v2))
+
+      //filter test
+      val evenPredicate_v2: Int => Boolean = _ % 2 == 0
+      println(intList.filter(evenPredicate_v2))
+
+      //flatmap test
+      val doublerList_v2: Int => LList[Int] = x => Cons(x, Cons(x * 2, Empty()))
+      println(intList.flatMap(doublerList_v2))
+
+      //foreach test
+      val printHeart: Int => Unit = x => print(s"$x <3 ")
+      intList.foreach(printHeart)
+
+      //sort test
+      val intList_v2: LList[Int] = Cons(5, Cons(3, Cons(1, Cons(8, Empty()))))
+      val aSorter: (Int, Int) => Int = _ - _
+      println(intList_v2.sort(aSorter))
+    }
 }
 
 
