73ee1e19ed71116b9ba2fb0f22b3af346871ff79
Camilla H. Urdal <camillahaugenurdal@gmail.com>
2025-01-06 12:03:11 +0100

    Update MapFlatMapFilterFor.scala

--- src/main/scala/workbook/part3fp/MapFlatMapFilterFor.scala
+++ src/main/scala/workbook/part3fp/MapFlatMapFilterFor.scala
@@ -23,12 +23,25 @@ object MapFlatMapFilterFor {
   val colors = List("black", "white", "red")
 
   def superList(n: List[Int], c: List[Char], s: List[String]):List[String] = {
-    val newList = n.flatMap()
+      n.flatMap(nElem => c.flatMap(cElem => s.map(sElem => s"\n$nElem$cElem - $sElem")))
   }
 
+  //same as superList- use for-comprehensions for better readability
+  val combinations = numbers.flatMap(number => chars.flatMap(char => colors.map(color => s"\n$number$char - $color")))
+
+  //for-comprehensions
+
+  val combinationsFor = for {
+    number <- numbers
+    char <- chars
+    color <- colors
+  } yield s"$number$char - $color"
+    // for every number in numbers, every char in chars and every color in colors yield(return) interpolated string
+    // and return them all under the same collection.
 
   def main(args: Array[String]): Unit = {
-
+//    print(combinations)
+    print(combinationsFor)
   }
 
 }
